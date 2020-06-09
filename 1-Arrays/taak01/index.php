529346e0ed33f67a369e5977ef43f30c4c33b3f6
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-06-09 11:24:03 +0200

    taak 1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,10 @@
+<?php
+$Piet = array("voornaam"=>"Piet", "achternaam"=>"Pettinga", "leeftijd"=>17, "klas"=>"9A");
+$Muhammed = array("voornaam"=>"Muhammed", "achternaam"=>"El hamid", "leeftijd"=>16, "klas"=>"8");
+$Els = array("voornaam"=>"Els", "achternaam"=>"Bekofig", "leeftijd"=>18, "klas"=>"10A");
+
+echo "Voornaam: ".$Piet["voornaam"]."<br>".
+    "Achternaam: ".$Piet["achternaam"]."<br>".
+    "Leeftijd: ".$Piet["leeftijd"]."<br>".
+    "klas: ".$Piet["klas"];
+?>
