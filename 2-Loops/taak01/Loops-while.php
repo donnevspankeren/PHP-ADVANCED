2db3e6f0d38bca183e63f0301f7266f18b7214c8
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-06-09 11:36:46 +0200

    loops taak 1

---
+++ 2-Loops/taak01/Loops-while.php
@@ -0,0 +1,13 @@
+<?php
+
+$leeftijd=0;
+
+while($leeftijd<=17){
+    echo "ik ben " . $leeftijd . " jaar. Dus mag ik nog niet stemmen." . "<br>";
+    $leeftijd++;
+}
+
+echo "ik ben 18 jaar dus heb ik stemrecht."
+
+
+?>
