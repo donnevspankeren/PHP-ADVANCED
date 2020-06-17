deaf6d32483b4b31847a31ed83705d0a1aae9f29
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-06-17 10:14:22 +0200

    froms taak 1

---
+++ 3-Forms/taak01/verwerk.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html>
+    <head>
+    </head> 
+    <body>  
+        <?php
+
+        echo "voornaam is: ".$_POST["Voornaam"]."<br>".
+            "Klas is: ".$_POST["Klas"]."<br>".
+            "Leeftijd is: ".$_POST["Leeftijd"]."<br>".
+            "Het adres is: ".$_POST["Adres"]."<br>".
+            "De plaatsnaam is: ".$_POST["Plaatsnaam"]."<br>".
+            "De favoriete muziekband is: ".$_POST["Muziekband"];  
+            
+        ?>
+    </body> 
+</html>
