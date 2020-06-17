851d09c8925592b37a430aa411adbe8352c27eac
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-06-17 12:36:28 +0200

    function taak 1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,24 @@
+<DOCTYPE! html>
+<html>
+<head>
+
+</head>
+<body>
+    <?php
+
+    function schrijfKleur($kleur){
+         echo "<div style= color:$kleur> Deze zin heeft de kleur $kleur</div>"."<br>";
+    }
+
+        schrijfkleur("red");
+        schrijfKleur("blue");
+        schrijfKleur("yellow");
+        schrijfKleur("grey");
+    ?>
+</body>
+
+
+
+
+
+</html>
