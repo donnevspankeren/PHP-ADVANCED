03a8b4188c5d63208fca5e811d438a4bb9fec801
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-06-19 10:46:39 +0200

    opdrahct 2 module php advance

--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -11,18 +11,26 @@
        
         <tr>
             <?php
-                $gegevens= array("voornaam"=> "donne", "leeftijd"=> 17, "woonplaats"=>"De Kwakel", "Sport"=>"Voetbal", "Opleiding"=> "Applicatie ontwikkelaar");
+                    $gegevens= array("voornaam"=> "donne", "leeftijd"=> 17, "woonplaats"=>"De Kwakel", "Sport"=>"Voetbal", "Opleiding"=> "Applicatie ontwikkelaar");
+                function maakRijTH($key,$value){
+                    echo "<th style=\"color:$_POST[tekstKleur];padding:$_POST[padding];border:$_POST[border];\">$key</th>";
+
+                }
                 foreach ($gegevens as $key => $value){
-                   echo "<th style=\"color:$_POST[tekstKleur];padding:$_POST[padding];border:$_POST[border];\">$key</th>";
+                    maakRijTH($key, $value);
                 }
                
             ?>
         </tr>
         <tr>
             <?php
+            function maakRijTD($key,$value){
+                echo "<td style=\"color:$_POST[tekstKleur];padding:$_POST[padding];border:$_POST[border];\">$value</td>";
+
+            }
                 $gegevens= array("voornaam"=> "donne", "leeftijd"=> 17, "woonplaats"=>"De Kwakel", "Sport"=>"Voetbal", "Opleiding"=> "Applicatie ontwikkelaar");
                 foreach ($gegevens as $key => $value){
-                   echo "<td style=\"color:$_POST[tekstKleur];padding:$_POST[padding];border:$_POST[border];\">$value</td>";
+                    maakRijTD($key,$value);
                 }
                 
             ?>
